e54f1de5804468f71244d1149be29b4e84275383
memoxmrdl <jmemox@gmail.com>
2013-05-11 21:34:03 -0500

    manager add producto

--- controller/registrar_producto.php
+++ controller/registrar_producto.php
@@ -1,26 +1,24 @@
 <?php
-    session_start();
-    require_once "../mysql.php";
-    require_once "../utilities.php";
+session_start();
+require_once "mysql.php";
+require_once "utilities.php";
 
-    $rq = @$_POST;
+$rq = @$_POST;
 
-    $r = "";
+$ms = new mysql();
 
-        $ms = new mysql();
+$SQL = "INSERT INTO productos (producto_id,tipo_cantidades_id,nombre,descripcion,precio,url,almacen) VALUES ";
+$SQL .= "(null, ".$rq["tipo_cantidades_id"].",'".$rq["nombre"]."', '".$rq["descripcion"]."', ";
+$SQL .= "".$rq["precio"].", '".$rq["url"]."', '".$rq["almacen"]."')";
 
-            $SQL = "INSERT INTO productos (producto_id,nombre,descripcion,precio,url,almacen) VALUES ";
-            $SQL .= "(null, '".$rq["nombre"]."', '".$rq["descripcion"]."', ";
-            $SQL .= "'".$rq["precio"]."', '".$rq["url"]."', '".$rq["almacen"]."')";
+$ms->query($SQL);
 
-            $ms->query($SQL);
+if($ms->error()) {
+    set_notice("Ya existe un producto o hubo problemas al registrar", 2);
+    $ms->Rollback();
+} else {
+    set_notice("Producto registrado", 3);
+    $ms->Commit();
+}
 
-            if($ms->error()) {
-                set_notice("Ya existe un producto o hubo problemas al registrar", 2);
-                $ms->Rollback();
-            } else {
-                set_notice("Producto registrado", 3);
-                $ms->Commit();
-            }
-
-    header("location: ../admin/index.php");
+header("location: ../admin/index.php");
