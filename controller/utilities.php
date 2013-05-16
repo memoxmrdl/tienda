6df82d2ac9ec39ed6b2dccbb25dedab09199f1f1
memoxmrdl <jmemox@gmail.com>
2013-05-15 23:37:36 -0500

    class cart

--- controller/utilities.php
+++ controller/utilities.php
@@ -40,35 +40,5 @@ function set_notice($message, $type) {
 
 
 class Cart {
-  function setitem($i, $v) {
-      $_SESSION['items'][$i] = $v;
-  } 
-
-  function getitem($i) {
-      return $_SESSION['items'][$i];
-  }
-
-  function all() {
-      return $_SESSION['items'];
-  }
-
-  function size() {
-      return count($_SESSION['items']);
-  }
-
-  function add_item($artnr, $num) {
-      $this->setitem($artnr, $this->size()+1);
-  }
-
-  function remove_item($artnr, $num) {
-      if ($this->getitem($artnr) > $num) {
-          $this->setitem($artnr, $this->size()-1);
-          return true;
-      } elseif ($this->getitem($artnr) == $num) {
-          unset($this->getitem($artnr));
-          return true;
-      } else {
-          return false;
-      }
-  }
+  function size() {}
 }
